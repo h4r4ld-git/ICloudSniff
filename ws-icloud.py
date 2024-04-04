e8d1d56b5d3b7f5fd91be5a4f6e36825ccc1752c
mr-2104 <mr-2104@kali.voo.be>
2024-04-04 05:00:25 +0200

    DNS captures

--- ws-icloud.py
+++ ws-icloud.py
@@ -1,8 +1,17 @@
 import pyshark
 
 cap = pyshark.FileCapture('linux-firefox.pcapng')
+
 dns = {}
+dest = []
 for packet in cap:
+    # Partie destinataires
+    if "ip" in dir(packet) and packet.ip.dst not in dest:
+        dest.append(packet.ip.dst)
+    if "ipv6" in dir(packet) and packet.ipv6.dst not in dest:
+        dest.append(packet.ipv6.dst)
+    
+    # Partie DNS
     if "dns" in dir(packet):
         if "resp_name" in dir(packet.dns):
             if packet.dns.resp_name not in dns:
@@ -10,4 +19,17 @@ for packet in cap:
                     dns[packet.dns.aaaa] = packet.dns.resp_name
                 if "a" in dir(packet.dns):
                     dns[packet.dns.a] = packet.dns.resp_name
-print(dns)
+print("Destinataires\n")
+for d in dest:
+    if d in dns.keys():
+        print(f"\t{d}\t{dns[d]}")
+
+
+for funct in ["Collab-file-linux.pcapng", "DNS_Connection_Opera.pcapng", "download-upload-linux.pcapng"]: 
+    cap = pyshark.FileCapture(funct)
+    s = 0
+    t = 0
+    for packet in cap:
+        s += int(packet.frame_info.len)*8
+        t = packet.frame_info.time_relative
+    print(funct, s, t)
